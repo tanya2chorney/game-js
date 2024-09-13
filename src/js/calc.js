ac19f47ffbe087d37b79758a6238c6e89c213fcf
Artem <vsezdes021@gmail.com>
2024-09-13 19:09:47 +0300

    added calculator

--- src/js/calc.js
+++ src/js/calc.js
@@ -1,39 +1,44 @@
 let currentOperation = null;
 
-    function setOperation(operation) {
-        currentOperation = operation;
+const operationButtons = document.querySelectorAll('.operation');
+
+operationButtons.forEach(button => {
+  button.addEventListener('click', function () {
+    currentOperation = this.getAttribute('data-operation');
+  });
+});
+
+const equalBtn = document.querySelector('.equal');
+
+function calculate() {
+  const number1 = parseFloat(document.getElementById('number1').value);
+  const number2 = parseFloat(document.getElementById('number2').value);
+  let result = '';
+
+  if (isNaN(number1) || isNaN(number2)) {
+    result = 'Введіть числа';
+  } else if (currentOperation === null) {
+    result = 'Оберіть операцію';
+  } else {
+    switch (currentOperation) {
+      case '+':
+        result = number1 + number2;
+        break;
+      case '-':
+        result = number1 - number2;
+        break;
+      case '*':
+        result = number1 * number2;
+        break;
+      case '/':
+        result = number2 !== 0 ? number1 / number2 : 'Ділення на нуль';
+        break;
+      default:
+        result = 'Невідома операція';
     }
+  }
 
-    const equalBtn = document.querySelector('.equal');
+  document.getElementById('result').value = result;
+}
 
-    function calculate() {
-        const number1 = parseFloat(document.getElementById('number1').value);
-        const number2 = parseFloat(document.getElementById('number2').value);
-        let result = '';
-
-        if (isNaN(number1) || isNaN(number2)) {
-            result = 'Введіть числа';
-        } else if (currentOperation === null) {
-            result = 'Оберіть операцію';
-        } else {
-            switch (currentOperation) {
-                case '+':
-                    result = number1 + number2;
-                    break;
-                case '-':
-                    result = number1 - number2;
-                    break;
-                case '*':
-                    result = number1 * number2;
-                    break;
-                case '/':
-                    result = number2 !== 0 ? number1 / number2 : 'Ділення на нуль';
-                    break;
-                default:
-                    result = 'Невідома операція';
-            }
-        }
-
-        document.getElementById('result').value = result;
-    }
-    equalBtn.addEventListener('click', calculate);
+equalBtn.addEventListener('click', calculate);
