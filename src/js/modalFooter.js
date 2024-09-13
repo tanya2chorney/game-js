368fd79c24f00a6087e8ffdcbdb1cc87e56716c4
Artem <vsezdes021@gmail.com>
2024-09-13 19:54:26 +0300

    added footer modal

---
+++ src/js/modalFooter.js
@@ -0,0 +1,24 @@
+const refs = {
+    openModalBtn: document.querySelector('[data-action="open-modal"]'),
+    closeModalBtn: document.querySelector('[data-action="close-modal"]'),
+    backdrop: document.querySelector(".backdrop"),
+  };
+  
+  function openModal() {
+    document.body.classList.add("show-modal");
+  }
+  
+  refs.openModalBtn.addEventListener("click", openModal);
+  
+  function closeModal() {
+    document.body.classList.remove("show-modal");
+  }
+  
+  function closeBackdrop(event) {
+    if (event.target === event.currentTarget) {
+      closeModal();
+    }
+  }
+  
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.backdrop.addEventListener("click", closeBackdrop);
